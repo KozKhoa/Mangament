a8067b9b7f5a98cfa851ccff00bd665217716455
kozkhoa <khoahuynh787898@gmail.com>
2025-12-21 11:37:33 +0700

    update card grid

--- client/components/grids/card-grid.tsx
+++ client/components/grids/card-grid.tsx
@@ -81,7 +81,7 @@ export default function CardGrid({
             // Grid
             <div
               ref={childRef}
-              className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-2
+              className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-2
                 border-b-2 border-foreground pb-2 w-full"
             >
               {children}
