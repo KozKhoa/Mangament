9258a814e329be4350b5238d96bc46eefe1dd705
kozkhoa <khoahuynh787898@gmail.com>
2026-01-11 19:09:20 +0700

    update app json

--- server/App.js
+++ server/App.js
@@ -15,7 +15,7 @@ import authorRoute from "./src/routes/Author.Route.js";
 import genreRoute from "./src/routes/Genre.Route.js";
 import { initRedis } from "./src/configs/redis.js";
 
-initRedis();
+// initRedis();
 
 console.log("App is running with NODE ENV =", process.env.NODE_ENV);
 
