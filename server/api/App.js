3e11a83e743843d6b3b71718a4c8964eb5f9ceb7
kozkhoa <khoahuynh787898@gmail.com>
2026-04-09 20:36:51 +0700

    feat: increase express request body size limits to 10mb

--- server/api/App.js
+++ server/api/App.js
@@ -239,7 +239,20 @@ app.get("/openapi.json", (req, res) => {
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(cookieParser());
-app.use(express.json());
+
+app.use(
+  express.json({
+    limit: "10mb",
+  }),
+);
+
+app.use(
+  express.urlencoded({
+    limit: "10mb",
+    extended: true,
+  }),
+);
+
 app.use(RequestLogger);
 
 // Let render ping after amount of time for preventing container to idle
