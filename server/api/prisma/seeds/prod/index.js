d007953a6bfb698daffcc1d92e544e4d2afa8d0c
kozkhoa <khoahuynh787898@gmail.com>
2026-09-21 14:11:09 +0700

    feat: enhance seeding process with environment-specific commands and add genre, image, nation, and user seeders

---
+++ server/api/prisma/seeds/prod/index.js
@@ -0,0 +1,14 @@
+import * as nationSeeding from "./nations.js";
+import * as userSeeding from "./users.js";
+import * as imageSeeding from "./images.js";
+import * as genreSeeding from "./genre.js";
+
+// await imageSeeding.default();
+
+await nationSeeding.default();
+
+// await genreSeeding.default();
+
+await userSeeding.default();
+
+process.exit(0);
